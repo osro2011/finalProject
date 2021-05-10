dd78a901790f3426cb091e5e6cf29f9ee4d0a745
Oscar Rosenlund <banan@pop-os.localdomain>
2021-05-10 15:12:10 +0200

    made a working pong and began working on netcode

--- index.js
+++ index.js
@@ -5,17 +5,38 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+lobbies = [];
+queue = [];
+
+function Lobby(p1, p2) {
+    this.host = p1;
+    this.visitor = p2;
+
+    this.host.emit("assign", "host");
+    this.visitor.emit("assign", "visitor");
+
+    this.host.on("event", (msg) => {
+        console.log("event recieved");
+        this.visitor.emit("update", msg);
+    });
+    this.visitor.on("event", (msg) => {
+        console.log("event recieved 2");
+        this.host.emit("update", msg);
+    });
+}
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('update', (msg) => {
-        console.log(msg);
-    })
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+    console.log("new connection")
+    queue.push(socket);
+    if (queue.length % 2 == 0) {
+        lobbies.push(new Lobby(queue.pop(), queue.pop()));
+    }
+    socket.on("disconnect", (reason) => {
+        console.log("disconnected " + reason);
     })
 });
 
